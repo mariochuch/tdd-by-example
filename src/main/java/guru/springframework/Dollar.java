9389b65b91264afca9be700af63441a65f439323
mariuszchucherko <mariuszchucherko@workhuman.com>
2023-05-03 13:48:57 +0100

    18.Degenerate Objects

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -6,8 +6,9 @@ public class Dollar {
         this.amount = amount;
     }
 
-    void times(int multiplier){
-        amount *= multiplier;
+    Dollar times(int multiplier){
+
+        return new Dollar(amount * multiplier);
 
     }
 }
