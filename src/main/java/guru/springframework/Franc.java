cfb03793432533de571f9d8d50a5cc81bce27ad1
mariuszchucherko <mariuszchucherko@workhuman.com>
2023-05-04 08:27:12 +0100

    21.Franc-ly Speaking

---
+++ src/main/java/guru/springframework/Franc.java
@@ -0,0 +1,30 @@
+package guru.springframework;
+
+import java.util.Objects;
+
+public class Franc {
+    private final int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier){
+
+        return new Franc(amount * multiplier);
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Franc franc = (Franc) o;
+        return amount == franc.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
