6a1ebbf0847caea3cb86d27f2d15605649301735
mariuszchucherko <mariuszchucherko@workhuman.com>
2023-05-08 15:35:27 +0100

    25.Times We're Livin' In

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -3,14 +3,14 @@ package guru.springframework;
 import java.util.Objects;
 
 public class Dollar extends Money {
-
-    public Dollar(int amount) {
-        this.amount = amount;
+    public Dollar(int amount, String currency) {
+        super(amount,currency);
     }
+
 
     public Money times(int multiplier){
 
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
 
     }
 
